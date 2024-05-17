781f14c32b09e96af38f65f8f3d6880689a0d3b4
lgy <ini03134@naver.com>
2024-05-17 10:35:36 +0900

    initial commit

---
+++ src/main/java/com/example/hi_project/HiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hi_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiProjectApplication.class, args);
+	}
+
+}
